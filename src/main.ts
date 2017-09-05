cbcd3e95e9a83e0f0b111afb0d368c8c382006b4
Adirelle <adirelle+github@gmail.com>
2017-09-06 00:17:03 +0200

    Converted the main loop.

--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,141 @@
-import * as CreepManager from "./components/creeps/creepManager";
-import * as Config from "./config/config";
+import * as Config from './config/config';
 
-import * as Profiler from "screeps-profiler";
-import { log } from "./lib/logger/log";
+import * as Profiler from 'screeps-profiler';
+import { log } from './lib/logger/log';
 
-// Any code written outside the `loop()` method is executed only when the
-// Screeps system reloads your script.
-// Use this bootstrap wisely. You can cache some of your stuff to save CPU.
-// You should extend prototypes before the game loop executes here.
+import { Factory as RoleFactory, Population, registry as Roles } from './components/roles';
 
-// This is an example for using a config variable from `config.ts`.
-// NOTE: this is used as an example, you may have better performance
-// by setting USE_PROFILER through webpack, if you want to permanently
-// remove it on deploy
-// Start the profiler
 if (Config.USE_PROFILER) {
   Profiler.enable();
 }
 
-log.info(`Scripts bootstrapped`);
+log.info('Scripts bootstrapped');
+
 if (__REVISION__) {
   log.info(`Revision ID: ${__REVISION__}`);
 }
 
-function mloop() {
-  // Check memory for null or out of bounds custom objects
-  if (!Memory.uuid || Memory.uuid > 100) {
-    Memory.uuid = 0;
-  }
+function mloop(): void {
+  cleanCreepMemory();
+  _.each(Game.rooms, spawnCreeps);
+  _.each(Game.creeps, runCreep);
+}
 
-  for (const i in Game.rooms) {
-    const room: Room = Game.rooms[i];
-
-    CreepManager.run(room);
-
-    // Clears any non-existing creep memory.
-    for (const name in Memory.creeps) {
-      const creep: any = Memory.creeps[name];
-
-      if (creep.room === room.name) {
-        if (!Game.creeps[name]) {
-          log.info("Clearing non-existing creep memory:", name);
-          delete Memory.creeps[name];
-        }
-      }
+function cleanCreepMemory(): void {
+  for (const name in Memory.creeps) {
+    if (!Game.creeps[name]) {
+      Memory.creeps[name] = undefined;
     }
   }
+}
+
+function runCreep(creep: Creep): void {
+  if (!creep.spawning) {
+    const role = Roles.reload(creep);
+    role.run();
+  }
+}
+
+type BodyDef = string[];
+
+interface SpawnInfo {
+  spawn: StructureSpawn;
+  body: BodyDef;
+  size: number;
+}
+
+function spawnCreeps(room: Room): void {
+  const pop = countCreepsByRole(room);
+  _.each(Roles.factories, (f: RoleFactory) => spawnRoleCreeps(room, f, pop));
+}
+
+function countCreepsByRole(room: Room): Population {
+  const count: Population = {};
+  _.each(Roles.factories, (f: RoleFactory) => (count[f.name] = 0));
+  _.each(room.find<Creep>(FIND_MY_CREEPS), (c: Creep) => count[c.memory.role]++);
+  return count;
+}
+
+function spawnRoleCreeps(room: Room, role: RoleFactory, pop: Population) {
+  let target = role.targetPopulation(room, pop);
+  const wish = target;
+  let current = pop[role.name];
+  const prev = current;
+
+  if (role.dependsOn) {
+    _.each(role.dependsOn, (factor: number, name: string) => {
+      const limit = Math.ceil(pop[name] * factor);
+      if (limit < target) {
+        log.info(`${role.name} limited from ${target} to ${limit} by `);
+        target = limit;
+      }
+    });
+  }
+
+  while (current < target && spawnCreep(room, role, role.bodyTemplate)) {
+    current++;
+  }
+
+  log.info(`spawnRoleCreep ${role.name} ${prev}/${wish} => ${current}/${target}`);
+}
+
+function spawnCreep(room: Room, role: RoleFactory, bodyTpl: BodyDef) {
+  const biggest = biggestBody(room, bodyTpl);
+  if (biggest === null) {
+    return null;
+  }
+
+  const creepName = biggest.spawn.createCreep(biggest.body);
+  if (typeof creepName !== 'string') {
+    log.warning(`Could not spawn ${role.name} in room ${room.name}: ${creepName}`);
+    return null;
+  }
+
+  const creep = Game.creeps[creepName];
+  creep.memory.role = role.name;
+  log.info(`Spawning ${role.name} in room ${room.name}, name: ${creepName}, body: ${biggest.body}`);
+  return creep;
+}
+
+function biggestBody(room: Room, bodyTpl: BodyDef): SpawnInfo|null {
+  const allSpawns = room.find<StructureSpawn>(FIND_MY_STRUCTURES, {
+    filter: (s: StructureSpawn) => s.structureType === STRUCTURE_SPAWN && !s.spawning
+  });
+  let size = 0;
+  let body = null;
+  let spawn = null;
+  _.each(allSpawns, (testSpawn: StructureSpawn) => {
+    let testSize = 1;
+    do {
+      const testBody = repeatBody(bodyTpl, testSize);
+      const result = testSpawn.canCreateCreep(testBody);
+      log.debug(`${testSpawn.name} ${testBody} => ${result}`);
+      if (result !== OK) {
+        break;
+      }
+      if (testSize > size) {
+        spawn = testSpawn;
+        size = testSize;
+        body = testBody;
+      }
+      testSize++;
+    } while (true);
+  });
+  log.debug(`biggestBody: ${bodyTpl} => ${size} ${body} ${spawn}`);
+  if (spawn !== null && body !== null) {
+    return { spawn, body, size };
+  }
+  return null;
+}
+
+function repeatBody(tpl: BodyDef, size: number) {
+  const body: BodyDef = [];
+  _.each(tpl, (part: string) => {
+    for (let j = 0; j < size; j++) {
+      body.push(part);
+    }
+  });
+  return body;
 }
 
 /**
@@ -56,4 +146,8 @@ function mloop() {
  *
  * @export
  */
-export const loop = !Config.USE_PROFILER ? mloop : () => { Profiler.wrap(mloop); };
+export const loop = !Config.USE_PROFILER
+  ? mloop
+  : () => {
+      Profiler.wrap(mloop);
+    };
