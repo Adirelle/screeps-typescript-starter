42b02cb22ee33f2ad5ddc6da124cbafda7a18e61
Adirelle <adirelle+github@gmail.com>
2017-09-14 08:44:54 +0200

    Properly declare log global.

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,8 +1,3 @@
-// add objects to `global` here
-declare namespace NodeJS {
-  interface Global {
-    log: any;
-  }
 }
 
 interface Memory {
@@ -30,6 +25,28 @@ interface RoomPosition {
 interface Structure {
   toString(): string;
 }
+
+declare const enum LogLevels {
+  ERROR,
+  WARNING,
+  INFO,
+  DEBUG
+}
+
+interface Logger {
+  level: LogLevels;
+  showSource: boolean;
+  showTick: boolean;
+
+  trace(error: Error): Logger;
+
+  error(...args: any[]): void;
+  warning(...args: any[]): void;
+  info(...args: any[]): void;
+  debug(...args: any[]): void;
+}
+
+declare const log: Logger;
 
 declare const __REVISION__: string;
 declare const __BUILD_TIME__: string;
