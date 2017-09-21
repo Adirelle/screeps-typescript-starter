6e0425642da2297e22996c11fec282d3fa6345b0
Adirelle <adirelle+github@gmail.com>
2017-09-21 21:18:27 +0200

    Catch individual errors when managing creeps.

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -4,8 +4,17 @@ import { resetTasks } from './tasks';
 export function manageTasks(room: Room) {
   resetTasks(room);
   for (const c of room.myCreeps) {
-    for (let i = 0; iterate(c) && i < 3; i++) {
-      // spin
+    for (let i = 0; i < 3; i++) {
+      try {
+        if (!iterate(c)) {
+          break;
+        }
+      } catch (ex) {
+        log.error(`Error while managing ${c}`);
+        log.trace(ex);
+        c.memory.state = 'default';
+        delete c.memory.value;
+      }
     }
   }
 }
